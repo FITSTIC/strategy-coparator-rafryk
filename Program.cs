cc7486b094e2793d9a204f2ecdc5170f22e383dd
Raffaele Balzani <raffaele.balzani@alice.it>
2017-11-06 10:35:12 +0100

    add progetto

--- Program.cs
+++ Program.cs
@@ -6,83 +6,141 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            var nipote = new Person(6, "Luca");
-            var nonno = new Person(75, "Aristide");
+          
+            var nipote = new Person(6, "Luca", "Cippo");
+            var nonno = new Person(45, "Aristide", "Merotti");
+            var mario = new Person(35, "Mario", "Perotti");
+            var gino = new Person(33, "Gino", "Negro");
+            var lorenzo = new Person(1, "Lorenzo", "Verratti");
+            var marcello = new Person(21, "Lorenzo", "Ciccio");
+            var davide = new Person(100, "Davide", "Basso");
+          
+
+            Person[] arrayPerson = {nipote,nonno,mario,gino,lorenzo,marcello,davide};
 
             var comparator = new PersonComparator();
 
-            Person[] listAge = comparator.Compare(nipote, nonno, new AgeComparator());
-            Console.WriteLine("La persona più giovane è :"+listAge[0].name);//Luca
-
-            Person[] listName = comparator.Compare(nipote, nonno, new NameComparator());
-            Console.WriteLine("La prima persona in ordine alfabetico è :"+listName[0].name);//Aristide
-
+            // Età Comparator
+            Person[] listAge = comparator.Compare(arrayPerson, new AgeComparator());
+            //Console.WriteLine("La persona più giovane è :"+listAge[0].name);//Luca Cippo
+            Console.WriteLine("Classifica ordinata per l'età:");
+            //Console.WriteLine(listAge.Length);
+            for (int i = 0; i < listAge.Length; i++) {
+              Console.WriteLine(listAge[i].ToString());
+            }
+            // Nome Comparator
+            Person[] listFirstName = comparator.Compare(arrayPerson, new NameComparator());
+            //Console.WriteLine("La prima persona in ordine di nome è :"+listFirstName[0].firstName);//Aristide Merotti
+            Console.WriteLine("Classifica ordinata per il nome");
+            for (int i = 0; i < listFirstName.Length; i++) {
+              Console.WriteLine(listFirstName[i].ToString());
+            }
             Console.ReadLine();
         }
     }
 
+    /*
+    * Comparatore della Età
+    */
     class AgeComparator : IPersonStrategy
     {
-        public Person[] Sort(Person p1, Person p2)
+        public Person[] Sort(Person[] arrayPerson)
         {
-            Person[] result = new Person[2];
+            Person[] result = new Person[arrayPerson.Length];
+            //Bubble Sort
+            int i;
+            int j;
+            Person temp;
 
-            if(p1.age > p2.age){
-                result[0] = p2;
-                result[1] = p1;
-            }else{
-                result[0] = p1;
-                result[1] = p2;
+            for (i = (arrayPerson.Length - 1); i >= 0; i--)
+            {
+              for (j = 1; j <= i; j++)
+              {
+                if (arrayPerson[j - 1].age > arrayPerson[j].age)
+                {
+                  temp = arrayPerson[j - 1];
+                  arrayPerson[j - 1] = arrayPerson[j];
+                  arrayPerson[j] = temp;
+                }
+              }
             }
-
+            result = arrayPerson;
             return result;
         }
     }
-
+    /*
+    * Comparatore del Nome e Cognome
+    */
     class NameComparator : IPersonStrategy
     {
-        public Person[] Sort(Person p1, Person p2)
+        public Person[] Sort(Person[] arrayPerson)
         {
-            Person[] result = new Person[2];
+            Person temp;
+            Person[] result = new Person[arrayPerson.Length];
+            
 
-            if(p1.name.CompareTo(p2.name) > 0){
-                result[0] = p2;
-                result[1] = p1;
-            }else{
-                result[0] = p1;
-                result[1] = p2;
+
+            for (int i = (arrayPerson.Length - 1); i >= 0; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (arrayPerson[j-1].firstName.CompareTo(arrayPerson[j].firstName) > 0)
+                    {
+                        temp = arrayPerson[j - 1];
+                        arrayPerson[j - 1] = arrayPerson[j];
+                        arrayPerson[j] = temp;
+                     
+                    }
+                    else if (arrayPerson[j - 1].firstName.CompareTo(arrayPerson[j].firstName) == 0)
+                    {
+                        if (arrayPerson[j - 1].lastName.CompareTo(arrayPerson[j].lastName) > 0)
+                        {
+                            temp = arrayPerson[j - 1];
+                            arrayPerson[j - 1] = arrayPerson[j];
+                            arrayPerson[j] = temp;
+                        }
+                    }
+                }
             }
-
+            result = arrayPerson;
             return result;
         }
     }
+    
 
     interface IPersonStrategy
     {
-        Person[] Sort(Person p1, Person p2);
+        Person[] Sort(Person[] array);
     }
 
+    //Comparatore delle Persone
     class PersonComparator
     {
-        public Person[] Compare(Person p1, Person p2, IPersonStrategy strategy){
+        public Person[] Compare(Person[] arrayPerson, IPersonStrategy strategy){
             Person[] pArray;
-
-            pArray = strategy.Sort(p1, p2);
-
+            pArray = strategy.Sort(arrayPerson);
             return pArray;
 
         }
     }
 
+    //Classe Persona con attributi: Età, Nome, Cognome
     class Person
     {
         public int age;
-        public string name;
+        public string firstName;
+        public string lastName;
 
-        public Person(int a, string n)
+        public Person(int a, string firstName, string lastName)
         {
-            age = a;
-            name = n;
+            this.age = a;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            return this.firstName + " " + this.lastName + " " + this.age;
         }
     }
 }
